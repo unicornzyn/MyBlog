0221abbc81eff98661992ab0d314db8a73e07825
unicornzyn <hawk_zyn@163.com>
2018-03-07 08:57:19 +0800

    fixed bug

--- MyProject/Controllers/AccountController.cs
+++ MyProject/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ using MyProject.Models;
 namespace MyProject.Controllers
 {
     [Authorize]
-    public class AccountController : Controller
+    public class AccountController : BaseController
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
--- MyProject/Controllers/ManageController.cs
+++ MyProject/Controllers/ManageController.cs
@@ -11,7 +11,7 @@ using MyProject.Models;
 namespace MyProject.Controllers
 {
     [Authorize]
-    public class ManageController : Controller
+    public class ManageController : BaseController
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
